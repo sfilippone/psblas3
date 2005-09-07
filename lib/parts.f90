02fd9346ca9f4221b481463ab3f4809342e50199
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2005-09-07 09:20:43 +0000

    Should not keep generated files.

--- lib/parts.f90
+++
@@ -1,7 +0,0 @@
-interface 
-   !.....user passed subroutine.....
-   subroutine parts(glob_index,nrow,np,pv,nv)
-     integer, intent (in)  :: glob_index,np,nrow
-     integer, intent (out) :: nv, pv(*)
-   end subroutine parts
-end interface
