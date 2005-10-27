64202e10235b6a886552558a2b1c8569517448b0
Alfredo Buttari <alfredo.buttari@enseeiht.fr>
2005-10-27 12:55:16 +0000

    *** empty log message ***

---
+++ src/modules/psb_all_mod.f90
@@ -0,0 +1,18 @@
+module psb_all_mod
+  ! the interface modules
+  use psb_tools_mod
+  use psb_const_mod
+  use psb_prec_mod
+  use psb_methd_mod
+  use psb_serial_mod
+  use psb_blacs_mod
+  use psb_comm_mod
+  use psb_error_mod
+  use psb_psblas_mod
+
+  ! the types
+  use psb_spmat_type
+  use psb_descriptor_type
+  use psb_prec_type
+
+end module psb_all_mod
