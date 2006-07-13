81555629389d946e1d34ee879ef1861f5cd0b34b
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2006-07-13 17:08:01 +0000

    *** empty log message ***

--- src/modules/extra_env.f90
+++ src/modules/extra_env.f90
@@ -1,13 +1,15 @@
 subroutine psb_set_coher(ictxt,isvch)
   integer :: ictxt, isvch
   ! Ensure global coherence for convergence checks.
-  ! Do nothing: ESSL does coherence by default,
-  ! and does not handle req=16 
-!!$  Call blacs_get(ictxt,16,isvch)
-!!$  Call blacs_set(ictxt,16,1)
+  Call blacs_get(ictxt,16,isvch)
+  Call blacs_set(ictxt,16,1)
 end subroutine psb_set_coher
 subroutine psb_restore_coher(ictxt,isvch)
   integer :: ictxt, isvch
   ! Ensure global coherence for convergence checks.
-!!$  Call blacs_set(ictxt,16,isvch)
+  Call blacs_set(ictxt,16,isvch)
 end subroutine psb_restore_coher
+subroutine psb_get_mpicomm(ictxt,comm)
+  integer :: ictxt, comm
+  call blacs_get(ictxt,10,comm)
+end subroutine psb_get_mpicomm
