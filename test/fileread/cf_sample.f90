7c874c0be521a0c693cfde29fb2a3d3767baa053
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2012-12-07 09:46:01 +0000

    *** empty log message ***

--- test/fileread/cf_sample.f90
+++ test/fileread/cf_sample.f90
@@ -1,4 +1,4 @@
-!!$ 
+ !!$ 
 !!$              Parallel Sparse BLAS  version 3.0
 !!$    (C) Copyright 2006, 2007, 2008, 2009, 2010, 2012
 !!$                       Salvatore Filippone    University of Rome Tor Vergata
