cdeea6ff06d4a1c1f29b8a15d690b676298c85f6
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2013-11-13 09:48:24 +0000

    *** empty log message ***

--- base/modules/psb_const_mod.F90
+++ base/modules/psb_const_mod.F90
@@ -71,10 +71,10 @@ module psb_const_mod
   ! 
   ! Version
   !
-  character(len=*), parameter    :: psb_version_string_ = "3.0.0"
+  character(len=*), parameter    :: psb_version_string_ = "3.1.2"
   integer(psb_ipk_), parameter   :: psb_version_major_  = 3
-  integer(psb_ipk_), parameter   :: psb_version_minor_  = 0
-  integer(psb_ipk_), parameter   :: psb_patchlevel_     = 0
+  integer(psb_ipk_), parameter   :: psb_version_minor_  = 1
+  integer(psb_ipk_), parameter   :: psb_patchlevel_     = 2
 
   !
   !     Handy & miscellaneous constants
