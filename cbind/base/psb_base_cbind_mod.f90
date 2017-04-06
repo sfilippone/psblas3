b07eb6dbbc585679c688dbc36ea58e9b23746274
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2017-04-06 15:36:53 +0000

    psblas3-mcbind:  cbind/base/psb_base_cbind_mod.f90  cbind/base/psb_d_comm_cbind.f90  cbind/base/psb_d_comm_cbind_mod.f90
    Start of comm, fix name mismatch

--- cbind/base/psb_base_cbind_mod.f90
+++ cbind/base/psb_base_cbind_mod.f90
@@ -6,5 +6,6 @@ module psb_base_cbind_mod
   use psb_d_tools_cbind_mod
   use psb_c_tools_cbind_mod
   use psb_z_tools_cbind_mod  
+  use psb_d_comm_cbind_mod
   use psb_base_psblas_cbind_mod
 end module psb_base_cbind_mod
