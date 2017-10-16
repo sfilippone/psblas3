7d18d330e05c86eeeae3c98493a8364293a158e7
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2017-10-16 15:12:24 +0100

    Take away genpde module from util, fold into test.

--- test/pargen/psb_s_pde2d.f90
+++ test/pargen/psb_s_pde2d.f90
@@ -56,51 +56,315 @@
 ! data distribution.
 !
 module psb_s_pde2d_mod
+
+  use psb_base_mod, only : psb_spk_, psb_ipk_, psb_desc_type,&
+       &  psb_sspmat_type, psb_s_vect_type, szero,&
+       &  psb_s_base_sparse_mat, psb_s_base_vect_type, psb_i_base_vect_type
+
+
+  interface 
+    function s_func_2d(x,y) result(val)
+      import :: psb_spk_
+      real(psb_spk_), intent(in) :: x,y
+      real(psb_spk_) :: val
+    end function s_func_2d
+  end interface
+
+  interface psb_gen_pde2d 
+    module procedure psb_s_gen_pde2d
+  end interface psb_gen_pde2d
+
 contains
+
+  function s_null_func_2d(x,y) result(val)
+
+    real(psb_spk_), intent(in) :: x,y
+    real(psb_spk_) :: val
+
+    val = szero
+
+  end function s_null_func_2d
+
+
   !
-  ! functions parametrizing the differential equation 
-  !  
-  function b1(x,y)
-    use psb_base_mod, only : psb_spk_
-    real(psb_spk_) :: b1
-    real(psb_spk_), intent(in) :: x,y
-    b1=1.e0/sqrt(2.e0)
-  end function b1
-  function b2(x,y)
-    use psb_base_mod, only : psb_spk_
-    real(psb_spk_) ::  b2
-    real(psb_spk_), intent(in) :: x,y
-    b2=1.e0/sqrt(2.e0)
-  end function b2
-  function c(x,y)
-    use psb_base_mod, only : psb_spk_
-    real(psb_spk_) ::  c
-    real(psb_spk_), intent(in) :: x,y
-    c=0.e0
-  end function c
-  function a1(x,y)
-    use psb_base_mod, only : psb_spk_
-    real(psb_spk_) ::  a1   
-    real(psb_spk_), intent(in) :: x,y
-    a1=1.e0/80
-  end function a1
-  function a2(x,y)
-    use psb_base_mod, only : psb_spk_
-    real(psb_spk_) ::  a2
-    real(psb_spk_), intent(in) :: x,y
-    a2=1.e0/80
-  end function a2
-  function g(x,y)
-    use psb_base_mod, only : psb_spk_, sone, szero
-    real(psb_spk_) ::  g
-    real(psb_spk_), intent(in) :: x,y
-    g = szero
-    if (x == sone) then
-      g = sone
-    else if (x == szero) then 
-      g = exp(-y**2)
+  !  subroutine to allocate and fill in the coefficient matrix and
+  !  the rhs. 
+  !
+  subroutine psb_s_gen_pde2d(ictxt,idim,a,bv,xv,desc_a,afmt,&
+       & a1,a2,b1,b2,c,g,info,f,amold,vmold,imold,nrl)
+    use psb_base_mod
+    !
+    !   Discretizes the partial differential equation
+    ! 
+    !   a1 dd(u)  a2 dd(u)    b1 d(u)  b2 d(u) 
+    ! -   ------ -  ------ +  -----  +  ------  + c u = f
+    !      dxdx     dydy         dx       dy     
+    !
+    ! with Dirichlet boundary conditions
+    !   u = g 
+    !
+    !  on the unit square  0<=x,y<=1.
+    !
+    !
+    ! Note that if b1=b2=c=0., the PDE is the  Laplace equation.
+    !
+    implicit none
+    procedure(s_func_2d)  :: b1,b2,c,a1,a2,g
+    integer(psb_ipk_)     :: idim
+    type(psb_sspmat_type) :: a
+    type(psb_s_vect_type) :: xv,bv
+    type(psb_desc_type)   :: desc_a
+    integer(psb_ipk_)     :: ictxt, info
+    character(len=*)      :: afmt
+    procedure(s_func_2d), optional :: f
+    class(psb_s_base_sparse_mat), optional :: amold
+    class(psb_s_base_vect_type), optional :: vmold
+    class(psb_i_base_vect_type), optional :: imold
+    integer(psb_ipk_), optional :: nrl
+
+    ! Local variables.
+
+    integer(psb_ipk_), parameter :: nb=20
+    type(psb_s_csc_sparse_mat)  :: acsc
+    type(psb_s_coo_sparse_mat)  :: acoo
+    type(psb_s_csr_sparse_mat)  :: acsr
+    real(psb_spk_)           :: zt(nb),x,y,z
+    integer(psb_ipk_) :: m,n,nnz,glob_row,nlr,i,ii,ib,k
+    integer(psb_ipk_) :: ix,iy,iz,ia,indx_owner
+    integer(psb_ipk_) :: np, iam, nr, nt
+    integer(psb_ipk_) :: icoeff
+    integer(psb_ipk_), allocatable     :: irow(:),icol(:),myidx(:)
+    real(psb_spk_), allocatable :: val(:)
+    ! deltah dimension of each grid cell
+    ! deltat discretization time
+    real(psb_spk_)            :: deltah, sqdeltah, deltah2
+    real(psb_spk_), parameter :: rhs=0.e0,one=1.e0,zero=0.e0
+    real(psb_dpk_)    :: t0, t1, t2, t3, tasb, talc, ttot, tgen, tcdasb
+    integer(psb_ipk_) :: err_act
+    procedure(s_func_2d), pointer :: f_
+    character(len=20)  :: name, ch_err,tmpfmt
+
+    info = psb_success_
+    name = 'create_matrix'
+    call psb_erractionsave(err_act)
+
+    call psb_info(ictxt, iam, np)
+
+
+    if (present(f)) then 
+      f_ => f
+    else
+      f_ => s_null_func_2d
     end if
-  end function g
+
+    deltah   = 1.e0/(idim+2)
+    sqdeltah = deltah*deltah
+    deltah2  = 2.e0* deltah
+
+    ! initialize array descriptor and sparse matrix storage. provide an
+    ! estimate of the number of non zeroes 
+
+    m   = idim*idim
+    n   = m
+    nnz = ((n*7)/(np))
+    if(iam == psb_root_) write(psb_out_unit,'("Generating Matrix (size=",i0,")...")')n
+
+    if (present(nrl)) then 
+      nr = nrl
+    else
+      !
+      ! Using a simple BLOCK distribution.
+      !
+      nt = (m+np-1)/np
+      nr = max(0,min(nt,m-(iam*nt)))
+    end if
+
+    nt = nr
+    call psb_sum(ictxt,nt) 
+    if (nt /= m) then 
+      write(psb_err_unit,*) iam, 'Initialization error ',nr,nt,m
+      info = -1
+      call psb_barrier(ictxt)
+      call psb_abort(ictxt)
+      return    
+    end if
+    call psb_barrier(ictxt)
+    t0 = psb_wtime()
+    call psb_cdall(ictxt,desc_a,info,nl=nr)
+    if (info == psb_success_) call psb_spall(a,desc_a,info,nnz=nnz)
+    ! define  rhs from boundary conditions; also build initial guess 
+    if (info == psb_success_) call psb_geall(xv,desc_a,info)
+    if (info == psb_success_) call psb_geall(bv,desc_a,info)
+    call psb_barrier(ictxt)
+    talc = psb_wtime()-t0
+
+    if (info /= psb_success_) then
+      info=psb_err_from_subroutine_
+      ch_err='allocation rout.'
+      call psb_errpush(info,name,a_err=ch_err)
+      goto 9999
+    end if
+
+    ! we build an auxiliary matrix consisting of one row at a
+    ! time; just a small matrix. might be extended to generate 
+    ! a bunch of rows per call. 
+    ! 
+    allocate(val(20*nb),irow(20*nb),&
+         &icol(20*nb),stat=info)
+    if (info /= psb_success_ ) then 
+      info=psb_err_alloc_dealloc_
+      call psb_errpush(info,name)
+      goto 9999
+    endif
+
+
+    myidx = desc_a%get_global_indices()
+    nlr = size(myidx)
+
+    ! loop over rows belonging to current process in a block
+    ! distribution.
+
+    call psb_barrier(ictxt)
+    t1 = psb_wtime()
+    do ii=1, nlr,nb
+      ib = min(nb,nlr-ii+1) 
+      icoeff = 1
+      do k=1,ib
+        i=ii+k-1
+        ! local matrix pointer 
+        glob_row=myidx(i)
+        ! compute gridpoint coordinates
+        if (mod(glob_row,(idim)) == 0) then
+          ix = glob_row/(idim)
+        else
+          ix = glob_row/(idim)+1
+        endif
+        iy = (glob_row-(ix-1)*idim)
+        ! x, y
+        x = (ix-1)*deltah
+        y = (iy-1)*deltah
+
+        zt(k) = f_(x,y)
+        ! internal point: build discretization
+        !   
+        !  term depending on   (x-1,y)
+        !
+        val(icoeff) = -a1(x,y)/sqdeltah-b1(x,y)/deltah2
+        if (ix == 1) then 
+          zt(k) = g(szero,y)*(-val(icoeff)) + zt(k)
+        else
+          icol(icoeff) = (ix-2)*idim+iy
+          irow(icoeff) = glob_row
+          icoeff       = icoeff+1
+        endif
+        !  term depending on     (x,y-1)
+        val(icoeff)  = -a2(x,y)/sqdeltah-b2(x,y)/deltah2
+        if (iy == 1) then 
+          zt(k) = g(x,szero)*(-val(icoeff))   + zt(k)
+        else
+          icol(icoeff) = (ix-1)*idim+(iy-1)
+          irow(icoeff) = glob_row
+          icoeff       = icoeff+1
+        endif
+
+        !  term depending on     (x,y)
+        val(icoeff)=2.e0*(a1(x,y) + a2(x,y))/sqdeltah + c(x,y)
+        icol(icoeff) = (ix-1)*idim+iy
+        irow(icoeff) = glob_row
+        icoeff       = icoeff+1                  
+        !  term depending on     (x,y+1)
+        val(icoeff)=-a2(x,y)/sqdeltah+b2(x,y)/deltah2
+        if (iy == idim) then 
+          zt(k) = g(x,sone)*(-val(icoeff))   + zt(k)
+        else
+          icol(icoeff) = (ix-1)*idim+(iy+1)
+          irow(icoeff) = glob_row
+          icoeff       = icoeff+1
+        endif
+        !  term depending on     (x+1,y)
+        val(icoeff)=-a1(x,y)/sqdeltah+b1(x,y)/deltah2
+        if (ix==idim) then 
+          zt(k) = g(sone,y)*(-val(icoeff))   + zt(k)
+        else
+          icol(icoeff) = (ix)*idim+(iy)
+          irow(icoeff) = glob_row
+          icoeff       = icoeff+1
+        endif
+
+      end do
+      call psb_spins(icoeff-1,irow,icol,val,a,desc_a,info)
+      if(info /= psb_success_) exit
+      call psb_geins(ib,myidx(ii:ii+ib-1),zt(1:ib),bv,desc_a,info)
+      if(info /= psb_success_) exit
+      zt(:)=0.e0
+      call psb_geins(ib,myidx(ii:ii+ib-1),zt(1:ib),xv,desc_a,info)
+      if(info /= psb_success_) exit
+    end do
+
+    tgen = psb_wtime()-t1
+    if(info /= psb_success_) then
+      info=psb_err_from_subroutine_
+      ch_err='insert rout.'
+      call psb_errpush(info,name,a_err=ch_err)
+      goto 9999
+    end if
+
+    deallocate(val,irow,icol)
+
+    call psb_barrier(ictxt)
+    t1 = psb_wtime()
+    call psb_cdasb(desc_a,info,mold=imold)
+    tcdasb = psb_wtime()-t1
+    call psb_barrier(ictxt)
+    t1 = psb_wtime()
+    if (info == psb_success_) then 
+      if (present(amold)) then 
+        call psb_spasb(a,desc_a,info,dupl=psb_dupl_err_,mold=amold)
+      else
+        call psb_spasb(a,desc_a,info,dupl=psb_dupl_err_,afmt=afmt)
+      end if
+    end if
+    call psb_barrier(ictxt)
+    if(info /= psb_success_) then
+      info=psb_err_from_subroutine_
+      ch_err='asb rout.'
+      call psb_errpush(info,name,a_err=ch_err)
+      goto 9999
+    end if
+    if (info == psb_success_) call psb_geasb(xv,desc_a,info,mold=vmold)
+    if (info == psb_success_) call psb_geasb(bv,desc_a,info,mold=vmold)
+    if(info /= psb_success_) then
+      info=psb_err_from_subroutine_
+      ch_err='asb rout.'
+      call psb_errpush(info,name,a_err=ch_err)
+      goto 9999
+    end if
+    tasb = psb_wtime()-t1
+    call psb_barrier(ictxt)
+    ttot = psb_wtime() - t0 
+
+    call psb_amx(ictxt,talc)
+    call psb_amx(ictxt,tgen)
+    call psb_amx(ictxt,tasb)
+    call psb_amx(ictxt,ttot)
+    if(iam == psb_root_) then
+      tmpfmt = a%get_fmt()
+      write(psb_out_unit,'("The matrix has been generated and assembled in ",a3," format.")')&
+           &   tmpfmt
+      write(psb_out_unit,'("-allocation  time : ",es12.5)') talc
+      write(psb_out_unit,'("-coeff. gen. time : ",es12.5)') tgen
+      write(psb_out_unit,'("-desc asbly  time : ",es12.5)') tcdasb
+      write(psb_out_unit,'("- mat asbly  time : ",es12.5)') tasb
+      write(psb_out_unit,'("-total       time : ",es12.5)') ttot
+
+    end if
+    call psb_erractionrestore(err_act)
+    return
+
+9999 call psb_error_handler(ictxt,err_act)
+
+    return
+  end subroutine psb_s_gen_pde2d
 
 end module psb_s_pde2d_mod
 
@@ -360,6 +624,50 @@ contains
     write(iout,*)'               >= 1 do tracing every itrace'
     write(iout,*)'               iterations ' 
   end subroutine pr_usage
+  !
+  ! functions parametrizing the differential equation 
+  !  
+  function b1(x,y)
+    use psb_base_mod, only : psb_spk_
+    real(psb_spk_) :: b1
+    real(psb_spk_), intent(in) :: x,y
+    b1=1.e0/sqrt(2.e0)
+  end function b1
+  function b2(x,y)
+    use psb_base_mod, only : psb_spk_
+    real(psb_spk_) ::  b2
+    real(psb_spk_), intent(in) :: x,y
+    b2=1.e0/sqrt(2.e0)
+  end function b2
+  function c(x,y)
+    use psb_base_mod, only : psb_spk_
+    real(psb_spk_) ::  c
+    real(psb_spk_), intent(in) :: x,y
+    c=0.e0
+  end function c
+  function a1(x,y)
+    use psb_base_mod, only : psb_spk_
+    real(psb_spk_) ::  a1   
+    real(psb_spk_), intent(in) :: x,y
+    a1=1.e0/80
+  end function a1
+  function a2(x,y)
+    use psb_base_mod, only : psb_spk_
+    real(psb_spk_) ::  a2
+    real(psb_spk_), intent(in) :: x,y
+    a2=1.e0/80
+  end function a2
+  function g(x,y)
+    use psb_base_mod, only : psb_spk_, sone, szero
+    real(psb_spk_) ::  g
+    real(psb_spk_), intent(in) :: x,y
+    g = szero
+    if (x == sone) then
+      g = sone
+    else if (x == szero) then 
+      g = exp(-y**2)
+    end if
+  end function g
 
 end program psb_s_pde2d
 
