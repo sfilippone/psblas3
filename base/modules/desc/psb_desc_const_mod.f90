44ebca223543105181bb886a87940fe27107f5b9
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2018-04-04 16:31:20 +0100

    Added epk to desc_const_mod

--- base/modules/desc/psb_desc_const_mod.f90
+++ base/modules/desc/psb_desc_const_mod.f90
@@ -35,7 +35,7 @@
 !    Auxiliary module for descriptor: constant values. 
 !
 module psb_desc_const_mod
-  use psb_const_mod, only : psb_ipk_, psb_mpk_, psb_lpk_
+  use psb_const_mod, only : psb_ipk_, psb_lpk_, psb_mpk_, psb_epk_
   !
   !     Communication, prolongation & restriction
   !
