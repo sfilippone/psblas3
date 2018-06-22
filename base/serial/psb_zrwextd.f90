363fb97fd652cd676234c07c0c576d9bc0daa8fa
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2018-06-22 12:22:32 +0100

    Version with LX for RWEXTD.

--- base/serial/psb_zrwextd.f90
+++ base/serial/psb_zrwextd.f90
@@ -35,17 +35,18 @@
 !
 ! We have a problem here: 1. How to handle well all the formats? 
 !                         2. What should we do with rowscale? Does it only 
-!                            apply when a%fida='COO' ?????? 
+!                            apply when a%fmt()='COO' ?????? 
 !
 !
 subroutine psb_zrwextd(nr,a,info,b,rowscale)
-  use psb_base_mod, psb_protect_name => psb_zrwextd
+  use psb_mat_mod
+  use psb_serial_mod, psb_protect_name => psb_zrwextd
   implicit none
 
   ! Extend matrix A up to NR rows with empty ones (i.e.: all zeroes)
-  integer(psb_ipk_), intent(in)                          :: nr
+  integer(psb_ipk_), intent(in)                 :: nr
   type(psb_zspmat_type), intent(inout)        :: a
-  integer(psb_ipk_),intent(out)                          :: info
+  integer(psb_ipk_),intent(out)                 :: info
   type(psb_zspmat_type), intent(in), optional :: b
   logical,intent(in), optional                 :: rowscale
 
@@ -89,23 +90,20 @@ subroutine psb_zrwextd(nr,a,info,b,rowscale)
   call psb_erractionrestore(err_act)
   return
 
-9999 continue
-  call psb_erractionrestore(err_act)
-  if (err_act == psb_act_abort_) then
-     call psb_error()
-     return
-  end if
+9999 call psb_error_handler(err_act)
+
   return
 
 end subroutine psb_zrwextd
 subroutine psb_zbase_rwextd(nr,a,info,b,rowscale)
-  use psb_base_mod, psb_protect_name => psb_zbase_rwextd
+  use psb_mat_mod
+  use psb_serial_mod, psb_protect_name => psb_zbase_rwextd
   implicit none
 
   ! Extend matrix A up to NR rows with empty ones (i.e.: all zeroes)
-  integer(psb_ipk_), intent(in)                                :: nr
+  integer(psb_ipk_), intent(in)                        :: nr
   class(psb_z_base_sparse_mat), intent(inout)        :: a
-  integer(psb_ipk_),intent(out)                                :: info
+  integer(psb_ipk_),intent(out)                        :: info
   class(psb_z_base_sparse_mat), intent(in), optional :: b
   logical,intent(in), optional                       :: rowscale
 
@@ -236,12 +234,213 @@ subroutine psb_zbase_rwextd(nr,a,info,b,rowscale)
   call psb_erractionrestore(err_act)
   return
 
-9999 continue
-  call psb_erractionrestore(err_act)
-  if (err_act == psb_act_abort_) then
-    call psb_error()
-    return
-  end if
+9999 call psb_error_handler(err_act)
+
   return
 
 end subroutine psb_zbase_rwextd
+
+
+subroutine psb_lzrwextd(nr,a,info,b,rowscale)
+  use psb_mat_mod
+  use psb_serial_mod, psb_protect_name => psb_lzrwextd
+  implicit none
+
+  ! Extend matrix A up to NR rows with empty ones (i.e.: all zeroes)
+  integer(psb_lpk_), intent(in)                  :: nr
+  type(psb_lzspmat_type), intent(inout)        :: a
+  integer(psb_ipk_),intent(out)                  :: info
+  type(psb_lzspmat_type), intent(in), optional :: b
+  logical,intent(in), optional                 :: rowscale
+
+  integer(psb_lpk_) :: i,j,ja,jb,nza,nzb
+  integer(psb_ipk_) :: err_act
+  character(len=20)                 :: name, ch_err
+  type(psb_lz_coo_sparse_mat)     :: actmp
+  logical  rowscale_ 
+
+  name='psb_lzrwextd'
+  info  = psb_success_
+  call psb_erractionsave(err_act)
+
+  if (nr > a%get_nrows()) then 
+    select type(aa=> a%a) 
+    type is (psb_lz_csr_sparse_mat)
+      if (present(b)) then 
+        call psb_rwextd(nr,aa,info,b%a,rowscale)
+      else
+        call psb_rwextd(nr,aa,info,rowscale=rowscale)
+      end if
+    type is (psb_lz_coo_sparse_mat) 
+      if (present(b)) then 
+        call psb_rwextd(nr,aa,info,b%a,rowscale=rowscale)
+      else
+        call psb_rwextd(nr,aa,info,rowscale=rowscale)
+      end if
+    class default
+      call aa%mv_to_coo(actmp,info)
+      if (info == psb_success_) then 
+        if (present(b)) then 
+          call psb_rwextd(nr,actmp,info,b%a,rowscale=rowscale)
+        else
+          call psb_rwextd(nr,actmp,info,rowscale=rowscale)
+        end if
+      end if
+      if (info == psb_success_) call aa%mv_from_coo(actmp,info)
+    end select
+  end if
+  if (info /= psb_success_) goto 9999
+
+  call psb_erractionrestore(err_act)
+  return
+
+9999 call psb_error_handler(err_act)
+
+  return
+
+end subroutine psb_lzrwextd
+subroutine psb_lzbase_rwextd(nr,a,info,b,rowscale)
+  use psb_mat_mod
+  use psb_serial_mod, psb_protect_name => psb_lzbase_rwextd
+  implicit none
+
+  ! Extend matrix A up to NR rows with empty ones (i.e.: all zeroes)
+  integer(psb_lpk_), intent(in)                         :: nr
+  class(psb_lz_base_sparse_mat), intent(inout)        :: a
+  integer(psb_ipk_),intent(out)                         :: info
+  class(psb_lz_base_sparse_mat), intent(in), optional :: b
+  logical,intent(in), optional                       :: rowscale
+
+  integer(psb_lpk_) :: i,j,ja,jb,nza,nzb, ma, mb, na, nb
+  integer(psb_ipk_) :: err_act
+  character(len=20) :: name, ch_err
+  logical  rowscale_ 
+
+  name='psb_lzbase_rwextd'
+  info  = psb_success_
+  call psb_erractionsave(err_act)
+
+  if (present(rowscale)) then 
+    rowscale_ = rowscale
+  else
+    rowscale_ = .true.
+  end if
+
+  ma = a%get_nrows()
+  na = a%get_ncols()
+
+
+  select type(a) 
+  type is (psb_lz_csr_sparse_mat)
+
+    call psb_ensure_size(nr+1,a%irp,info)
+
+    if (present(b)) then 
+      mb = b%get_nrows()
+      nb = b%get_ncols()
+      nzb = b%get_nzeros()
+
+      select type (b) 
+      type is (psb_lz_csr_sparse_mat)
+        call psb_ensure_size(size(a%ja)+nzb,a%ja,info)
+        call psb_ensure_size(size(a%val)+nzb,a%val,info)
+        do i=1, min(nr-ma,mb)
+          a%irp(ma+i+1) =  a%irp(ma+i) + b%irp(i+1) - b%irp(i)
+          ja = a%irp(ma+i)
+          do jb = b%irp(i), b%irp(i+1)-1
+            a%val(ja) = b%val(jb)
+            a%ja(ja)  = b%ja(jb)
+            ja = ja + 1
+          end do
+        end do
+        do j=i,nr-ma
+          a%irp(ma+i+1) = a%irp(ma+i)
+        end do
+      class default 
+
+        write(psb_err_unit,*) 'Implement SPGETBLK in RWEXTD!!!!!!!'
+      end select
+      call a%set_ncols(max(na,nb))
+
+    else
+
+      do i=ma+2,nr+1
+        a%irp(i) = a%irp(i-1)
+      end do
+
+    end if
+
+    call a%set_nrows(nr)
+
+
+  type is (psb_lz_coo_sparse_mat) 
+    nza = a%get_nzeros()
+
+    if (present(b)) then 
+      mb = b%get_nrows()
+      nb = b%get_ncols()
+      nzb = b%get_nzeros()
+      call a%reallocate(nza+nzb)
+
+      select type(b)
+      type is (psb_lz_coo_sparse_mat) 
+
+        if (rowscale_) then 
+          do j=1,nzb
+            if ((ma + b%ia(j)) <= nr) then 
+              nza = nza + 1
+              a%ia(nza)  = ma + b%ia(j)
+              a%ja(nza)  = b%ja(j)
+              a%val(nza) = b%val(j)
+            end if
+          enddo
+        else
+          do j=1,nzb
+            if ((ma + b%ia(j)) <= nr) then 
+              nza = nza + 1
+              a%ia(nza)  = b%ia(j)
+              a%ja(nza)  = b%ja(j)
+              a%val(nza) = b%val(j)
+            end if
+          enddo
+        endif
+        call a%set_nzeros(nza)
+
+      type is (psb_lz_csr_sparse_mat) 
+        
+        do i=1, min(nr-ma,mb)
+          do jb = b%irp(i), b%irp(i+1)-1
+            nza = nza + 1 
+            a%val(nza) = b%val(jb)
+            a%ia(nza)  = ma + i
+            a%ja(nza)  = b%ja(jb)
+          end do
+        end do
+        call a%set_nzeros(nza)
+
+      class default 
+        write(psb_err_unit,*) 'Implement SPGETBLK in RWEXTD!!!!!!!'
+
+      end select
+
+      call a%set_ncols(max(na,nb))
+    endif
+
+    call a%set_nrows(nr)
+
+  class default 
+    info = psb_err_unsupported_format_
+    ch_err=a%get_fmt()
+    call psb_errpush(info,name,a_err=ch_err)
+    goto 9999
+
+  end select
+
+  call psb_erractionrestore(err_act)
+  return
+
+9999 call psb_error_handler(err_act)
+
+  return
+
+end subroutine psb_lzbase_rwextd
