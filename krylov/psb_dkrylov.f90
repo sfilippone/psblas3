5371e39c16d42f4497de68421542ff43b5a58bc8
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2018-12-03 10:52:40 +0000

    Accept GMRES as well as RGMRES.

--- krylov/psb_dkrylov.f90
+++ krylov/psb_dkrylov.f90
@@ -185,7 +185,7 @@ Subroutine psb_dkrylov_vect(method,a,prec,b,x,eps,desc_a,info,&
   case('BICGSTAB') 
     call  psb_dcgstab_vect(a,prec,b,x,eps,desc_a,info,&
          &itmax,iter,err,itrace,istop)
-  case('RGMRES')
+  case('RGMRES','GMRES')
     call  psb_drgmres_vect(a,prec,b,x,eps,desc_a,info,&
          &itmax,iter,err,itrace,irst,istop)
   case('BICGSTABL')
--- krylov/psb_zkrylov.f90
+++ krylov/psb_zkrylov.f90
@@ -185,7 +185,7 @@ Subroutine psb_zkrylov_vect(method,a,prec,b,x,eps,desc_a,info,&
   case('BICGSTAB') 
     call  psb_zcgstab_vect(a,prec,b,x,eps,desc_a,info,&
          &itmax,iter,err,itrace,istop)
-  case('RGMRES')
+  case('RGMRES','GMRES')
     call  psb_zrgmres_vect(a,prec,b,x,eps,desc_a,info,&
          &itmax,iter,err,itrace,irst,istop)
   case('BICGSTABL')
