43c1ccfc87fca71969cb86ec8d5a0c388ad90cf7
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2019-12-09 16:47:00 +0000

    Extract_dep_list new symm
    New dependency list symmetrizatoin, two alternatives: distributed, or
    replicated after allgather.

--- base/modules/desc/psb_indx_map_mod.f90
+++ base/modules/desc/psb_indx_map_mod.f90
@@ -313,17 +313,23 @@ module psb_indx_map_mod
 
   integer, parameter :: psi_symm_flag_norv_ = 0
   integer, parameter :: psi_symm_flag_inrv_ = 1
-  interface 
-    subroutine psi_symm_dep_list(rvsz,adj,idxmap,info,flag)
+  interface psi_symm_dep_list
+    subroutine psi_symm_dep_list_inrv(rvsz,adj,ictxt,info)
       import :: psb_indx_map, psb_ipk_, psb_lpk_, psb_mpk_
       implicit none 
-      integer(psb_mpk_), intent(inout) :: rvsz(:)
-      integer(psb_ipk_), intent(in)    :: adj(:)
-      class(psb_indx_map), intent(in)  :: idxmap
-      integer(psb_ipk_), intent(out)   :: info
-      integer(psb_ipk_), intent(in), optional :: flag
-    end subroutine psi_symm_dep_list
-  end interface
+      integer(psb_mpk_), intent(inout)   :: rvsz(0:)
+      integer(psb_ipk_), allocatable, intent(inout) :: adj(:)
+      integer(psb_ipk_), intent(in)      :: ictxt
+      integer(psb_ipk_), intent(out)     :: info
+    end subroutine psi_symm_dep_list_inrv
+    subroutine psi_symm_dep_list_norv(adj,ictxt,info)
+      import :: psb_indx_map, psb_ipk_, psb_lpk_, psb_mpk_
+      implicit none 
+      integer(psb_ipk_), allocatable, intent(inout) :: adj(:)
+      integer(psb_ipk_), intent(in)      :: ictxt
+      integer(psb_ipk_), intent(out)     :: info
+    end subroutine psi_symm_dep_list_norv    
+  end interface psi_symm_dep_list
 
 contains
 
