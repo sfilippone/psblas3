9870e5d371171f806adfe0e9b11fdcdd662489b8
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-04-16 11:07:28 +0200

    Corrected module load for util_cbind module

--- cbind/util/psb_s_util_cbind_mod.f90
+++ cbind/util/psb_s_util_cbind_mod.f90
@@ -1,59 +1,31 @@
-!
-!                Parallel Sparse BLAS  version 3.5
-!      (C) Copyright 2006-2018
-!        Salvatore Filippone
-!        Alfredo Buttari
-!
-!    Redistribution and use in source and binary forms, with or without
-!    modification, are permitted provided that the following conditions
-!    are met:
-!      1. Redistributions of source code must retain the above copyright
-!         notice, this list of conditions and the following disclaimer.
-!      2. Redistributions in binary form must reproduce the above copyright
-!         notice, this list of conditions, and the following disclaimer in the
-!         documentation and/or other materials provided with the distribution.
-!      3. The name of the PSBLAS group or the names of its contributors may
-!         not be used to endorse or promote products derived from this
-!         software without specific written permission.
-!
-!    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-!    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-!    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-!    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE PSBLAS GROUP OR ITS CONTRIBUTORS
-!    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-!    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-!    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-!    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-!    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-!    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-!    POSSIBILITY OF SUCH DAMAGE.
-!
-!
+module psb_sutil_cbind_mod
 
-module psb_s_util_cbind_mod
    use iso_c_binding
-   use psb_base_mod
    use psb_util_mod
+   use psb_base_mod
    use psb_objhandle_mod
    use psb_base_string_cbind_mod
 
 contains
 
     function psb_c_smm_mat_write(ah,matrixtitle,filename) bind(c) result(res)
+        use psb_base_mod
+        use psb_util_mod
+        use psb_base_string_cbind_mod
         implicit none
-        real(c_float) :: res
+        integer(psb_c_ipk_) :: res
 
         type(psb_c_sspmat)   :: ah
         character(c_char)      :: matrixtitle(*)
         character(c_char)      :: filename(*)
 
         type(psb_sspmat_type), pointer :: ap
-        character(1024)         :: mtitle
-        character(1024)         :: fname
+        character(len=1024)     :: mtitle
+        character(len=1024)     :: fname
         integer(psb_c_ipk_)     :: info
 
 
-        res = -1.0
+        res = -1
         if (c_associated(ah%item)) then
           call c_f_pointer(ah%item,ap)
         else
@@ -69,4 +41,4 @@ contains
 
     end function psb_c_smm_mat_write
 
-end module
+end module psb_sutil_cbind_mod
---
+++ cbind/util/psb_util_cbind_mod.f90
@@ -0,0 +1,6 @@
+module psb_base_util_cbind_mod
+  use psb_cutil_cbind_mod
+  use psb_dutil_cbind_mod
+  use psb_sutil_cbind_mod
+  use psb_zutil_cbind_mod
+end module psb_base_util_cbind_mod
