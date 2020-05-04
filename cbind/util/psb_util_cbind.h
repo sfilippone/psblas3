dbdbea9c6f5161d8fb1b0f077c1d15424012f14d
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-05-04 21:54:30 +0200

    C Interfaces for ijk2idx idx2ijk redefined

--- cbind/util/psb_util_cbind.h
+++ cbind/util/psb_util_cbind.h
@@ -6,8 +6,10 @@
 #include "psb_c_cutil.h"
 #include "psb_c_zutil.h"
 
-psb_i_t psb_c_idx2ijk(psb_i_t *i, psb_i_t *j, psb_i_t idx, psb_i_t nx, psb_i_t ny, psb_i_t base );
-psb_i_t psb_c_lidx2ijk(psb_i_t *i, psb_i_t *j, psb_l_t idx, psb_i_t nx, psb_i_t ny, psb_i_t base );
+psb_i_t psb_c_i_idx2ijk(psb_i_t ijk[],psb_i_t idx,psb_i_t sizes[],psb_i_t modes,psb_i_t base);
+psb_i_t psb_c_l_idx2ijk(psb_i_t ijk[],psb_l_t idx,psb_i_t sizes[],psb_i_t modes,psb_i_t base);
+psb_i_t psb_c_i_ijk2idx(psb_i_t ijk[],psb_i_t sizes[],psb_i_t modes,psb_i_t base);
+psb_l_t psb_c_l_ijk2idx(psb_i_t ijk[],psb_i_t sizes[],psb_i_t modes,psb_i_t base);
 
 
 #endif
