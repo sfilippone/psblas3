460eb44d463f279f5edb6e6f65ca26a961277b15
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-10-13 11:40:44 +0200

    Removed debug print

--- cbind/base/psb_c_dbase.c
+++ cbind/base/psb_c_dbase.c
@@ -4,27 +4,26 @@
 psb_c_dvector* psb_c_new_dvector()
 {
   psb_c_dvector* temp;
-  
+
   temp=(psb_c_dvector *) malloc(sizeof(psb_c_dvector));
   temp->dvector=NULL;
   return(temp);
 }
 
 psb_d_t* psb_c_dvect_get_cpy(psb_c_dvector *xh)
-{ 
+{
   psb_d_t *temp=NULL;
-  psb_i_t vsize=0; 
-  
-  if ((vsize=psb_c_dvect_get_nrows(xh))<0) 
+  psb_i_t vsize=0;
+
+  if ((vsize=psb_c_dvect_get_nrows(xh))<0)
     return(temp);
-  
-  if (vsize==0) 
+
+  if (vsize==0)
     vsize=1;
-  
+
   if ((temp=(psb_d_t *)malloc(vsize*sizeof(psb_d_t)))!=NULL)
     psb_c_dvect_f_get_cpy(temp,xh);
 
-  fprintf(stderr,"dvect_get_cpy: %lf\n",temp[0]);
   return(temp);
 
 }
@@ -33,7 +32,7 @@ psb_d_t* psb_c_dvect_get_cpy(psb_c_dvector *xh)
 psb_c_dspmat* psb_c_new_dspmat()
 {
   psb_c_dspmat* temp;
-  
+
   temp=(psb_c_dspmat *) malloc(sizeof(psb_c_dspmat));
   temp->dspmat=NULL;
   return(temp);
