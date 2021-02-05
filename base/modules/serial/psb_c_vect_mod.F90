493beaa751aef4aa4429c8c94b0a406c96a32356
Salvatore Filippone <salvatore.filippone@uniroma2.it>
2021-02-05 16:42:07 +0100

    Minor formatting

--- base/modules/serial/psb_c_vect_mod.F90
+++ base/modules/serial/psb_c_vect_mod.F90
@@ -1033,7 +1033,7 @@ contains
     ! Temp vectors
     type(psb_c_vect_type) :: wtemp
 
-    info = 0 
+    info = 0
     if( allocated(w%v) ) then
       if (.not.present(aux)) then
         allocate(wtemp%v, mold=w%v)
