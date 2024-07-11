681ea2fff762419ad1d70299f30507f99d18b46b
sfilippone <filippone.salvatore@gmail.com>
2024-07-11 12:10:14 +0200

    Updated docs

---
+++ rsb/impl/psb_d_cp_rsb_to_coo.f90
@@ -0,0 +1,77 @@
+!                Parallel Sparse BLAS   GPU plugin 
+!      (C) Copyright 2013
+!  
+!                         Salvatore Filippone
+!                         Alessandro Fanfarillo
+!   
+!    Redistribution and use in source and binary forms, with or without
+!    modification, are permitted provided that the following conditions
+!    are met:
+!      1. Redistributions of source code must retain the above copyright
+!         notice, this list of conditions and the following disclaimer.
+!      2. Redistributions in binary form must reproduce the above copyright
+!         notice, this list of conditions, and the following disclaimer in the
+!         documentation and/or other materials provided with the distribution.
+!      3. The name of the PSBLAS group or the names of its contributors may
+!         not be used to endorse or promote products derived from this
+!         software without specific written permission.
+!   
+!    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+!    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+!    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+!    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE PSBLAS GROUP OR ITS CONTRIBUTORS
+!    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+!    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+!    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+!    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+!    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+!    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+!    POSSIBILITY OF SUCH DAMAGE.
+!   
+  
+
+subroutine psb_d_cp_rsb_to_coo(a,b,info) 
+  
+  use psb_base_mod
+  use rsb
+  use psb_d_rsb_mat_mod, psb_protect_name => psb_d_cp_rsb_to_coo
+  implicit none 
+
+  class(psb_d_rsb_sparse_mat), intent(in)    :: a
+  class(psb_d_coo_sparse_mat), intent(inout) :: b
+  integer(psb_ipk_), intent(out)             :: info
+  real(psb_dpk_), pointer :: val_point(:)
+  type(c_ptr) :: t_p,s_p
+
+  !locals
+  integer(psb_ipk_)   :: i, j, k,nr,nza,nc
+
+  info = psb_success_
+
+  nr  = a%get_nrows()
+  nc  = a%get_ncols()
+  nza = a%get_nzeros()
+
+  call b%allocate(nr,nc,nza)
+  b%psb_d_base_sparse_mat = a%psb_d_base_sparse_mat
+
+  allocate(val_point(nza))
+
+  t_p = c_loc(val_point(1))
+
+  info = rsb_mtx_get_coo(a%rsbMat, t_p, b%ia, b%ja,RSB_FLAG_FORTRAN_INDICES_INTERFACE)
+
+  !info = rsb_mtx_switch_to_coo(a%rsbMat,t_p,b%ia,b%ja,RSB_FLAG_FORTRAN_INDICES_INTERFACE)
+
+  k = rsb_perror(s_p,info)
+
+  do i=1,nza
+     b%val(i)=val_point(i)
+  enddo
+
+  deallocate(val_point)
+
+  call b%set_nzeros(nza)
+  call b%fix(info)
+
+end subroutine psb_d_cp_rsb_to_coo
---
+++ rsb/rsb_int.h
@@ -0,0 +1,2 @@
+int Rsb_double_from_coo(void **rsbMat,double *va, int *ia,int *ja,int nnz,int nr,
+			int nc, int br, int bc);
