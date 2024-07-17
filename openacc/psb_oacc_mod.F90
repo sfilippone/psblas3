686bac42249fa58fc56c42dccc6b2b1b0e5a069c
sfilippone <filippone.salvatore@gmail.com>
2024-07-17 13:03:46 +0200

    Account for S/D/C/Z variants

--- openacc/psb_oacc_mod.F90
+++ openacc/psb_oacc_mod.F90
@@ -3,7 +3,15 @@ module psb_oacc_mod
 
     use psb_oacc_env_mod
 
+    use psb_i_oacc_vect_mod
+    use psb_s_oacc_vect_mod
     use psb_d_oacc_vect_mod
-    use psb_d_oacc_csr_mat_mod
+    use psb_c_oacc_vect_mod
+    use psb_z_oacc_vect_mod
 
+    use psb_s_oacc_csr_mat_mod
+    use psb_d_oacc_csr_mat_mod
+    use psb_c_oacc_csr_mat_mod
+    use psb_z_oacc_csr_mat_mod
+    
 end module psb_oacc_mod
