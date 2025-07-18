b51dc381409819abf5cc0bfad282bb1509cea3d1
Fabio Durastante <fabio.durastante@unipi.it>
2025-07-18 17:44:57 +0200

    Added C interface to initialize communicator from existing MPI communicator

--- cbind/base/psb_c_base.h
+++ cbind/base/psb_c_base.h
@@ -40,6 +40,7 @@ extern "C" {
 
   /* Environment routines */
   void    psb_c_init(psb_c_ctxt *cctxt);
+  void    psb_c_init_from_fint(psb_c_ctxt *cctxt, psb_i_t f_comm);
   void    psb_c_exit(psb_c_ctxt cctxt);
   void    psb_c_exit_ctxt(psb_c_ctxt cctxt);
   void    psb_c_abort(psb_c_ctxt cctxt);
