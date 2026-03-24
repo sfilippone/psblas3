fe53862126f8eec214ec7155e383f39c0abaa90f
sfilippone <filippone.salvatore@gmail.com>
2026-03-24 09:04:48 +0100

    Fix build dependencies 1

--- base/modules/comm/psi_c_comm_a_mod.f90
+++ base/modules/comm/psi_c_comm_a_mod.f90
@@ -31,7 +31,8 @@
 !    
 module psi_c_comm_a_mod
   use psi_penv_mod, only : psb_ctxt_type
-  use psb_desc_mod, only : psb_desc_type, psb_mpk_, psb_ipk_, psb_spk_, psb_i_base_vect_type
+  use psb_desc_mod, only : psb_desc_type, psb_mpk_, psb_ipk_, &
+       & psb_spk_, psb_i_base_vect_type
 
   interface psi_swapdata
     subroutine psi_cswapdatam(flag,n,beta,y,desc_a,work,info,data)
--- base/modules/comm/psi_m_comm_a_mod.f90
+++ base/modules/comm/psi_m_comm_a_mod.f90
@@ -31,7 +31,8 @@
 !    
 module psi_m_comm_a_mod
   use psi_penv_mod, only : psb_ctxt_type
-  use psb_desc_mod, only : psb_desc_type, psb_mpk_, psb_ipk_, psb_epk_
+  use psb_desc_mod, only : psb_desc_type, psb_mpk_, psb_ipk_, &
+       & psb_epk_, psb_i2pk_
 
   interface psi_swapdata
     subroutine psi_mswapdatam(flag,n,beta,y,desc_a,work,info,data)
