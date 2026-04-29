1cb7634d1fb1d069b786b24559f0d3b570be2a50
sfilippone <filippone.salvatore@gmail.com>
2026-04-29 14:06:23 +0200

    Fix GEMIN

--- base/psblas/psb_damax.f90
+++ base/psblas/psb_damax.f90
@@ -689,11 +689,11 @@ function psb_dmin_vect(x, desc_a, info,global) result(res)
     goto 9999
   end if
 
-  ! compute local max
+  ! compute local min
   if ((desc_a%get_local_rows() > 0).and.(m /= 0)) then
     res = x%minreal(desc_a%get_local_rows())
   else
-    res = dzero
+    res = HUGE(done)
   end if
 
   ! compute global min
