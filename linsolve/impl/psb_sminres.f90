026b1953c26e2d44e6e510e7f4dc92f54aa1e2b8
Stack-1 <simone.staccone@virgilio.it>
2026-06-11 20:49:29 +0200

    [FIX] Drop work= from MINRES vector calls after the development merge
    The MINRES implementation merged from development calls the vector psb_spmm
    and prec%apply with work=aux, but communication_v2 removed the work argument
    from the vector interfaces.  Remove work=aux from psb_{c,d,s,z}minres so the
    vector solvers match the communication_v2 interface; the library now builds.

--- linsolve/impl/psb_sminres.f90
+++ linsolve/impl/psb_sminres.f90
@@ -236,7 +236,7 @@ subroutine psb_sminres_vect(a,prec,b,x,eps,desc_a,info,&
 
    ! res = b - A*x
    call psb_geaxpby(sone,b,szero,res,desc_a,info)
-   if (info == psb_success_) call psb_spmm(-sone,a,x,sone,res,desc_a,info,work=aux)
+   if (info == psb_success_) call psb_spmm(-sone,a,x,sone,res,desc_a,info)
    if (info /= psb_success_) then
       info=psb_err_from_subroutine_non_
       call psb_errpush(info,name)
@@ -269,7 +269,7 @@ subroutine psb_sminres_vect(a,prec,b,x,eps,desc_a,info,&
    ! y  = beta1 * P' * v1, with v1 the first Lanczos vector.
    call psb_geaxpby(sone,res,szero,y,desc_a,info)
    if (info == psb_success_) call psb_geaxpby(sone,res,szero,r1,desc_a,info)
-   if (info == psb_success_) call prec%apply(res,y,desc_a,info,work=aux)
+   if (info == psb_success_) call prec%apply(res,y,desc_a,info)
    if (info /= psb_success_) then
       info=psb_err_from_subroutine_non_
       call psb_errpush(info,name)
@@ -354,7 +354,7 @@ subroutine psb_sminres_vect(a,prec,b,x,eps,desc_a,info,&
 
       s = sone/beta
       call psb_geaxpby(s,y,szero,v,desc_a,info)
-      if (info == psb_success_) call psb_spmm(sone,a,v,szero,y,desc_a,info,work=aux)
+      if (info == psb_success_) call psb_spmm(sone,a,v,szero,y,desc_a,info)
       if (itx >= 2 .and. info == psb_success_) then
          call psb_geaxpby((-beta/oldb),r1,sone,y,desc_a,info)
       end if
@@ -374,7 +374,7 @@ subroutine psb_sminres_vect(a,prec,b,x,eps,desc_a,info,&
       call psb_geaxpby((-alfa/beta),r2,sone,y,desc_a,info)
       if (info == psb_success_) call psb_geaxpby(sone,r2,szero,r1,desc_a,info)
       if (info == psb_success_) call psb_geaxpby(sone,y,szero,r2,desc_a,info)
-      if (info == psb_success_) call prec%apply(r2,y,desc_a,info,work=aux)
+      if (info == psb_success_) call prec%apply(r2,y,desc_a,info)
       if (info /= psb_success_) then
          info=psb_err_from_subroutine_non_
          call psb_errpush(info,name)
@@ -461,7 +461,7 @@ subroutine psb_sminres_vect(a,prec,b,x,eps,desc_a,info,&
       case(psb_istop_ani_)
          ! Compute true residual only for the ANI stopping criterion.
          call psb_geaxpby(sone,b,szero,res,desc_a,info)
-         if (info == psb_success_) call psb_spmm(-sone,a,x,sone,res,desc_a,info,work=aux)
+         if (info == psb_success_) call psb_spmm(-sone,a,x,sone,res,desc_a,info)
          if (info == psb_success_) rni = psb_geamax(res,desc_a,info)
          if (info == psb_success_) xni = psb_geamax(x,desc_a,info)
          errnum = rni
