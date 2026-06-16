64bee3fb7d1518a190a6c77057329a8bc9ecf52b
sfilippone <filippone.salvatore@gmail.com>
2026-06-16 16:42:07 +0200

    CBIND linsolve fixes

--- cbind/linsolve/psb_base_linsolve_cbind_mod.f90
+++ cbind/linsolve/psb_base_linsolve_cbind_mod.f90
@@ -6,6 +6,7 @@ module psb_base_linsolve_cbind_mod
   type, bind(c) :: solveroptions
     integer(psb_c_ipk_) :: iter, itmax, itrace, irst, istop
     real(c_double) ::  eps, err
+    type(psb_c_object_type)    ::  s1, s2
   end type solveroptions
 
 contains 
@@ -20,14 +21,15 @@ contains
     options%istop  = 2
     options%irst   = 10
     options%eps    = 1.d-6
-
+    options%s1     = psb_c_get_new_object()
+    options%s2     = psb_c_get_new_object()
     res            = 0
   end function psb_c_DefaultSolverOptions
 
   function psb_c_PrintSolverOptions(options)&
        &   bind(c,name='psb_c_PrintSolverOptions') result(res)
     implicit none 
-    type(solveroptions)   :: options
+    type(solveroptions), value   :: options
     integer(psb_c_ipk_) :: res
 
     write(*,*) 'PSBLAS C Interface Solver Options '
@@ -36,6 +38,8 @@ contains
     write(*,*) '   Stopping Criterion            :', options%istop
     write(*,*) '   Restart                       :', options%irst
     write(*,*) '   EPS (tolerance)               :', options%eps  
+    write(*,*) '   S1 scaling                    :', c_associated(options%s1%item)
+    write(*,*) '   S2 scaling                    :', c_associated(options%s2%item)
     res            = 0
   end function psb_c_PrintSolverOptions
     
