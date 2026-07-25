f9ac1291daf9e0f65646e8a75a25ffed36e4b99a
IvanDB <ivan.debiasi@phd.unipi.it>
2026-07-25 19:05:08 +0200

    Update mod files not template-generated

--- linsolve/psb_eigsolve_mod.f90
+++ linsolve/psb_eigsolve_mod.f90
@@ -1,10 +1,41 @@
 module psb_eigsolve_mod
   use psb_base_mod
   use psb_prec_mod
+  
   interface psb_powermethod
+    subroutine psb_s_powermethod(a, prec, lambda, desc, info, x, flag, itmax, iter, tol)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_sspmat_type, psb_dpk_, psb_s_vect_type
+      use psb_prec_mod, only : psb_sprec_type
+      type(psb_sspmat_type), intent(in)     :: a
+      class(psb_sprec_type), intent(inout)  :: prec 
+      real(psb_spk_), intent(out)           :: lambda
+      type(psb_desc_type), intent(in)       :: desc
+      integer(psb_ipk_), intent(out)        :: info
+      type(psb_s_vect_type), intent(inout), optional  :: x
+      logical, intent(in), optional                   :: flag
+      integer(psb_ipk_), intent(in), optional         :: itmax
+      integer(psb_ipk_), intent(out), optional        :: iter
+      real(psb_spk_), intent(in), optional            :: tol ! def 10^-3
+    end subroutine psb_s_powermethod
+
+    subroutine psb_c_powermethod(a, prec, lambda, desc, info, x, flag, itmax, iter, tol)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_cspmat_type, psb_spk_, psb_c_vect_type
+      use psb_prec_mod, only : psb_cprec_type
+      type(psb_cspmat_type), intent(in)     :: a
+      class(psb_cprec_type), intent(inout)  :: prec 
+      real(psb_spk_), intent(out)           :: lambda
+      type(psb_desc_type), intent(in)       :: desc
+      integer(psb_ipk_), intent(out)        :: info
+      type(psb_c_vect_type), intent(inout), optional  :: x
+      logical, intent(in), optional                   :: flag
+      integer(psb_ipk_), intent(in), optional         :: itmax
+      integer(psb_ipk_), intent(out), optional        :: iter
+      real(psb_spk_), intent(in), optional            :: tol ! def 10^-3
+    end subroutine psb_c_powermethod
+
     subroutine psb_d_powermethod(a, prec, lambda, desc, info, x, flag, itmax, iter, tol)
-      import :: psb_ipk_, psb_dpk_, psb_desc_type, &
-            psb_dspmat_type, psb_d_vect_type, psb_dprec_type
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_dspmat_type, psb_dpk_, psb_d_vect_type
+      use psb_prec_mod, only : psb_dprec_type
       type(psb_dspmat_type), intent(in)     :: a
       class(psb_dprec_type), intent(inout)  :: prec 
       real(psb_dpk_), intent(out)           :: lambda
@@ -15,10 +46,22 @@ module psb_eigsolve_mod
       integer(psb_ipk_), intent(in), optional         :: itmax
       integer(psb_ipk_), intent(out), optional        :: iter
       real(psb_dpk_), intent(in), optional            :: tol ! def 10^-3
-    end subroutine
+    end subroutine psb_d_powermethod
 
-    !TO DO: add s, c, z versions
-
+    subroutine psb_z_powermethod(a, prec, lambda, desc, info, x, flag, itmax, iter, tol)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_zspmat_type, psb_dpk_, psb_z_vect_type
+      use psb_prec_mod, only : psb_dprec_type
+      type(psb_zspmat_type), intent(in)     :: a
+      class(psb_zprec_type), intent(inout)  :: prec 
+      real(psb_dpk_), intent(out)           :: lambda
+      type(psb_desc_type), intent(in)       :: desc
+      integer(psb_ipk_), intent(out)        :: info
+      type(psb_z_vect_type), intent(inout), optional  :: x
+      logical, intent(in), optional                   :: flag
+      integer(psb_ipk_), intent(in), optional         :: itmax
+      integer(psb_ipk_), intent(out), optional        :: iter
+      real(psb_dpk_), intent(in), optional            :: tol ! def 10^-3
+    end subroutine psb_z_powermethod
   end interface
 end module psb_eigsolve_mod
  
--- linsolve/psb_linsolve_mod.f90
+++ linsolve/psb_linsolve_mod.f90
@@ -34,18 +34,15 @@
 !  Interfaces for linear solvers.
 !
 module psb_linsolve_mod
-
   use psb_const_mod
   public 
 
   interface psb_krylov
     subroutine psb_skrylov_vect(method, a, prec, b, x, eps, desc_a, info, &
-        & itmax, iter, err, itrace, irst, istop, cond)
-
-      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_sspmat_type, &
-            & psb_spk_, psb_s_vect_type
+                          & itmax, iter, err, itrace, irst, istop, cond, &
+                          & steps, base_type, eigext, Gram_solver, FGS_sweeps)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_sspmat_type, psb_spk_, psb_s_vect_type
       use psb_prec_mod, only : psb_sprec_type
-      
       character(len=*)                      :: method
       type(psb_sspmat_type), intent(in)     :: a
       type(psb_desc_type), intent(in)       :: desc_a
@@ -53,40 +50,43 @@ module psb_linsolve_mod
       type(psb_s_vect_type), intent(inout)  :: b
       type(psb_s_vect_type), intent(inout)  :: x
       real(psb_spk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)            :: info
-      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, irst, istop
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, irst, istop, steps
       integer(psb_ipk_), optional, intent(out)  :: iter
       real(psb_spk_), optional, intent(out)     :: err, cond
+      character, optional, intent(in)           :: base_type
+      real(psb_spk_), optional, intent(in)      :: eigext(2)
+      character(len=3), optional, intent(in)    :: Gram_solver
+      integer(psb_ipk_), optional, intent(in)   :: FGS_sweeps
     end subroutine psb_skrylov_vect
 
     subroutine psb_ckrylov_vect(method, a, prec, b, x, eps, desc_a, info, &
-        & itmax, iter, err, itrace, irst, istop, cond)
-
-      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_cspmat_type, &
-            & psb_spk_, psb_c_vect_type
+                          & itmax, iter, err, itrace, irst, istop, cond, &
+                          & steps, base_type, eigext, Gram_solver, FGS_sweeps)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_cspmat_type, psb_spk_, psb_c_vect_type
       use psb_prec_mod, only : psb_cprec_type
-      
       character(len=*)                      :: method
       type(psb_cspmat_type), intent(in)     :: a
       type(psb_desc_type), intent(in)       :: desc_a
       class(psb_cprec_type), intent(inout)  :: prec 
       type(psb_c_vect_type), intent(inout)  :: b
       type(psb_c_vect_type), intent(inout)  :: x
-      real(psb_spk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)            :: info
-      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, irst, istop
+      real(psb_spk), intent(in)            :: eps
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, irst, istop, steps
       integer(psb_ipk_), optional, intent(out)  :: iter
-      real(psb_spk_), optional, intent(out)     :: err, cond
+      real(psb_spk), optional, intent(out)     :: err, cond
+      character, optional, intent(in)           :: base_type
+      real(psb_spk), optional, intent(in)      :: eigext(2)
+      character(len=3), optional, intent(in)    :: Gram_solver
+      integer(psb_ipk_), optional, intent(in)   :: FGS_sweeps
     end subroutine psb_ckrylov_vect
 
     subroutine psb_dkrylov_vect(method, a, prec, b, x, eps, desc_a, info, &
                           & itmax, iter, err, itrace, irst, istop, cond, &
                           & steps, base_type, eigext, Gram_solver, FGS_sweeps)
-
-      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_dspmat_type, &
-            & psb_dpk_, psb_d_vect_type
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_dspmat_type, psb_dpk_, psb_d_vect_type
       use psb_prec_mod, only : psb_dprec_type
-
       character(len=*)                      :: method
       type(psb_dspmat_type), intent(in)     :: a
       type(psb_desc_type), intent(in)       :: desc_a
@@ -102,15 +102,13 @@ module psb_linsolve_mod
       real(psb_dpk_), optional, intent(in)      :: eigext(2)
       character(len=3), optional, intent(in)    :: Gram_solver
       integer(psb_ipk_), optional, intent(in)   :: FGS_sweeps
-    end subroutine psb_dkrylov_vect
+    end subroutine psb_zkrylov_vect
 
     subroutine psb_zkrylov_vect(method, a, prec, b, x, eps, desc_a, info, &
-        & itmax, iter, err, itrace, irst, istop, cond)
-      
-      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_zspmat_type, &
-            & psb_dpk_, psb_z_vect_type
+                          & itmax, iter, err, itrace, irst, istop, cond, &
+                          & steps, base_type, eigext, Gram_solver, FGS_sweeps)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_zspmat_type, psb_dpk_, psb_z_vect_type
       use psb_prec_mod, only : psb_zprec_type
-      
       character(len=*)                      :: method
       type(psb_zspmat_type), intent(in)     :: a
       type(psb_desc_type), intent(in)       :: desc_a
@@ -118,92 +116,80 @@ module psb_linsolve_mod
       type(psb_z_vect_type), intent(inout)  :: b
       type(psb_z_vect_type), intent(inout)  :: x
       real(psb_dpk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)            :: info
-      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, irst,istop
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, irst, istop, steps
       integer(psb_ipk_), optional, intent(out)  :: iter
       real(psb_dpk_), optional, intent(out)     :: err, cond
+      character, optional, intent(in)           :: base_type
+      real(psb_dpk_), optional, intent(in)      :: eigext(2)
+      character(len=3), optional, intent(in)    :: Gram_solver
+      integer(psb_ipk_), optional, intent(in)   :: FGS_sweeps
     end subroutine psb_zkrylov_vect
   end interface
 
   interface psb_richardson
-    subroutine psb_srichardson_vect(a,prec,b,x,eps,desc_a,info,&
-         & itmax,iter,err,itrace,istop)
-      
-      use psb_base_mod, only  : psb_ipk_, psb_desc_type, psb_sspmat_type, &
-           & psb_spk_, psb_s_vect_type
+    subroutine psb_srichardson_vect(a, prec, b, x, eps, desc_a, info, &
+                                  & itmax, iter, err, itrace, istop)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_sspmat_type, psb_spk_, psb_s_vect_type
       use psb_prec_mod, only : psb_sprec_type
-      
       type(psb_sspmat_type), intent(in)     :: a
-      type(psb_desc_type), intent(in)       :: desc_a
       class(psb_sprec_type), intent(inout)  :: prec 
       type(psb_s_vect_type), intent(inout)  :: b
       type(psb_s_vect_type), intent(inout)  :: x
       real(psb_spk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)                  :: info
-      integer(psb_ipk_), optional, intent(in)         :: itmax, itrace, istop
-      integer(psb_ipk_), optional, intent(out)        :: iter
-      real(psb_spk_), optional, intent(out) :: err
-
+      type(psb_desc_type), intent(in)       :: desc_a
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, istop
+      integer(psb_ipk_), optional, intent(out)  :: iter
+      real(psb_spk_), optional, intent(out)     :: err
     end subroutine psb_srichardson_vect
 
-    subroutine psb_crichardson_vect(a,prec,b,x,eps,desc_a,info,&
-         & itmax,iter,err,itrace,istop)
-      
-      use psb_base_mod, only  : psb_ipk_, psb_desc_type, psb_cspmat_type, &
-           & psb_spk_, psb_c_vect_type
+    subroutine psb_crichardson_vect(a, prec, b, x, eps, desc_a, info, &
+                                  & itmax, iter, err, itrace, istop)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_cspmat_type, psb_spk_, psb_c_vect_type
       use psb_prec_mod, only : psb_cprec_type
-      
       type(psb_cspmat_type), intent(in)     :: a
-      type(psb_desc_type), intent(in)       :: desc_a
       class(psb_cprec_type), intent(inout)  :: prec 
       type(psb_c_vect_type), intent(inout)  :: b
       type(psb_c_vect_type), intent(inout)  :: x
       real(psb_spk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)                  :: info
-      integer(psb_ipk_), optional, intent(in)         :: itmax, itrace, istop
-      integer(psb_ipk_), optional, intent(out)        :: iter
-      real(psb_spk_), optional, intent(out) :: err
-
+      type(psb_desc_type), intent(in)       :: desc_a
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, istop
+      integer(psb_ipk_), optional, intent(out)  :: iter
+      real(psb_spk_), optional, intent(out)     :: err
     end subroutine psb_crichardson_vect
 
-    subroutine psb_drichardson_vect(a,prec,b,x,eps,desc_a,info,&
-         & itmax,iter,err,itrace,istop)
-      
-      use psb_base_mod, only  : psb_ipk_, psb_desc_type, psb_dspmat_type, &
-           & psb_dpk_, psb_d_vect_type
+    subroutine psb_drichardson_vect(a, prec, b, x, eps, desc_a, info, &
+                                  & itmax, iter, err, itrace, istop)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_dspmat_type, psb_dpk_, psb_d_vect_type
       use psb_prec_mod, only : psb_dprec_type
-      
       type(psb_dspmat_type), intent(in)     :: a
-      type(psb_desc_type), intent(in)       :: desc_a
       class(psb_dprec_type), intent(inout)  :: prec 
       type(psb_d_vect_type), intent(inout)  :: b
       type(psb_d_vect_type), intent(inout)  :: x
       real(psb_dpk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)                  :: info
-      integer(psb_ipk_), optional, intent(in)         :: itmax, itrace, istop
-      integer(psb_ipk_), optional, intent(out)        :: iter
-      real(psb_dpk_), optional, intent(out) :: err
-
+      type(psb_desc_type), intent(in)       :: desc_a
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, istop
+      integer(psb_ipk_), optional, intent(out)  :: iter
+      real(psb_dpk_), optional, intent(out)     :: err
     end subroutine psb_drichardson_vect
 
-    subroutine psb_zrichardson_vect(a,prec,b,x,eps,desc_a,info,&
-         & itmax,iter,err,itrace,istop)
-      
-      use psb_base_mod, only  : psb_ipk_, psb_desc_type, psb_zspmat_type, &
-           & psb_dpk_, psb_z_vect_type
+    subroutine psb_zrichardson_vect(a, prec, b, x, eps, desc_a, info, &
+                                  & itmax, iter, err, itrace, istop)
+      use psb_base_mod, only : psb_ipk_, psb_desc_type, psb_zspmat_type, psb_dpk_, psb_z_vect_type
       use psb_prec_mod, only : psb_zprec_type
-      
       type(psb_zspmat_type), intent(in)     :: a
-      type(psb_desc_type), intent(in)       :: desc_a
       class(psb_zprec_type), intent(inout)  :: prec 
       type(psb_z_vect_type), intent(inout)  :: b
       type(psb_z_vect_type), intent(inout)  :: x
       real(psb_dpk_), intent(in)            :: eps
-      integer(psb_ipk_), intent(out)                  :: info
-      integer(psb_ipk_), optional, intent(in)         :: itmax, itrace, istop
-      integer(psb_ipk_), optional, intent(out)        :: iter
-      real(psb_dpk_), optional, intent(out) :: err
-
+      type(psb_desc_type), intent(in)       :: desc_a
+      integer(psb_ipk_), intent(out)        :: info
+      integer(psb_ipk_), optional, intent(in)   :: itmax, itrace, istop
+      integer(psb_ipk_), optional, intent(out)  :: iter
+      real(psb_dpk_), optional, intent(out)     :: err
     end subroutine psb_zrichardson_vect
   end interface
 end module psb_linsolve_mod
